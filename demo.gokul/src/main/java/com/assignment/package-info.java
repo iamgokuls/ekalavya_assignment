a4fa3f18072c442fde47f9d1ebf6785ffd82d537
Gokul S <Gokul S@DESKTOP-EVOF2PM>
2020-05-07 09:35:46 +0530

    Commit

--- demo.gokul/src/main/java/com/assignment/package-info.java
+++ demo.gokul/src/main/java/com/assignment/package-info.java
@@ -2,3 +2,4 @@ package com.assignment;
 
 //assignment 1: Display all subset of array [1,2,....9] whose sum is 9
 //assignment 2: For a given array and count k, reverse the first k elements,second k elements etc.... 
+//assignment 3: Design a simple calculator using factory method design pattern
